df0ea5fba9dc400eabd85255a36623862b1354f6
Boris <1smeha1@gmail.com>
2024-03-14 23:49:42 -0700

    Initial setup is done, updated README

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Fillout API application' });
 });
 
-// The endpoint for API
-router.get('/:formId/filteredResponses', (req, res) => {
-  const formId = req.params.formId;
-  const filters = JSON.parse(req.query.filters);
+router.get('/:formId/filteredResponse', function(req, res) {
+    // TEST render all params
+    res.send(req.params);
+    // TEST get formId
+    console.log(req.params.formId);
+    // TEST .env file
+    console.log(process.env.FILLOUT_API_KEY);
 
+    // TO DO
+    // const formId = req.params.formId;
+    // const filters = JSON.parse(req.query.filters);
 
-  // Filtered responses
-  res.json(filteredResponses);
+    // // Filtered responses
+    // res.json(filteredResponses);
 });
 
 // Start the ExpressJS server
